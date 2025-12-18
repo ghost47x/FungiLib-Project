e5dbb02c578a63c19a74add23ff9cd3ef27e0e59
Nara <molinasnani@gmail.com>
2025-12-18 23:43:13 +0800

    Add PATCH route for updating fungus
    added router patch

--- backend/routes/fungiRoutes.js
+++ backend/routes/fungiRoutes.js
@@ -7,6 +7,7 @@ const controller = require("../controllers/fungiController");
 router.get("/", controller.listFungi);
 router.get("/:id", controller.getFungus);
 router.post("/", upload.array("images", 5), controller.createFungus);
+router.patch("/:id", upload.array("images", 5), controller.updateFungus);
 router.delete("/:id", controller.deleteFungus);
 
 module.exports = router;
