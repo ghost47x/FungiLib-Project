ca3ceb94846bf4a700121dc392c184e47bbf3330
Saturnalli <129856415+Saturnalli@users.noreply.github.com>
2025-12-23 00:24:12 +0800

    Fixed import

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,9 +1,12 @@
-const admin = require('firebase-admin');
-const path = require('path');
-require('dotenv').config();
+const admin = require("firebase-admin");
+const path = require("path");
+require("dotenv").config();
 
-const serviceAccountPath = process.env.GOOGLE_APPLICATION_CREDENTIALS || './firebase-service-account.json';
-const serviceAccount = require(path.resolve(serviceAccountPath));
+const serviceAccountPath =
+  process.env.GOOGLE_APPLICATION_CREDENTIALS ||
+  path.join(__dirname, "../firebase-service-account.json");
+
+const serviceAccount = require(serviceAccountPath);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
